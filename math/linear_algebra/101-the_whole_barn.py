2dde74f9388e9d1ae483590190e8e3f7ff0dc9db
sergekamanzi <s.kamanzi@alustudent.com>
2024-09-12 11:21:20 -0700

    ln

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 '''
     A function def add_matrices(mat1, mat2):
-    that adds two matrices
+    that adds two matrices element-wise.
 '''
-
 
 def add_matrices(mat1, mat2):
     '''
         A function def add_matrices(mat1, mat2):
-        that adds two matrices
+        that adds two matrices element-wise.
     '''
     if isinstance(mat1, list) and isinstance(mat2, list):
         if len(mat1) == len(mat2):
             return [add_matrices(a, b) for a, b in zip(mat1, mat2)]
+        else:
+            return "None"  
     elif isinstance(mat1, (int, float)) and isinstance(mat2, (int, float)):
         return mat1 + mat2
-    return None
+    return "None"
