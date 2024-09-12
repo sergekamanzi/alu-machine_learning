050edbaef010570db2d89ac485acd1909b435790
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2024-09-12 20:09:34 +0200

    101-the_whole_barn.py

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
 '''
     A function def add_matrices(mat1, mat2):
-    that adds two matrices element-wise.
+    that adds two matrices
 '''
 
 
 def add_matrices(mat1, mat2):
     '''
         A function def add_matrices(mat1, mat2):
-        that adds two matrices element-wise.
+        that adds two matrices
     '''
     if isinstance(mat1, list) and isinstance(mat2, list):
-        if len(mat1) == len(mat2):  # Check if dimensions match
+        if len(mat1) == len(mat2):
             return [add_matrices(a, b) for a, b in zip(mat1, mat2)]
-        else:
-            return None  # Dimensions don't match
     elif isinstance(mat1, (int, float)) and isinstance(mat2, (int, float)):
-        return mat1 + mat2  # Base case: add numbers
-    return None  # If they don't match in type or structure
-
-# The function will return None if the matrices have mismatched dimensions.
-
+        return mat1 + mat2
+    return None
