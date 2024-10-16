c00b1c280496fa4ef9decf799ae6d707162eeb2a
sergekamanzi <s.kamanzi@alustudent.com>
2024-10-16 02:28:56 -0700

    100

--- math/bayesian_prob/100-continuous.py
+++ math/bayesian_prob/100-continuous.py
@@ -28,7 +28,7 @@ def posterior(x, n, p1, p2):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be a non-negative integer")
+        raise ValueError("x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(p1, float) or not isinstance(p2, float):
