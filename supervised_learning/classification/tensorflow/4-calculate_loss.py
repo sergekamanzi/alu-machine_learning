c71264bfd7a66884c801e4b8303f733f664cec94
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-01-26 20:13:24 +0200

    Delete supervised_learning/classification/tensorflow directory

--- supervised_learning/classification/tensorflow/4-calculate_loss.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" calcualte loss"""
-import tensorflow as tf
-
-
-def calculate_loss(y, y_pred):
-    """ loss function"""
-    return tf.losses.softmax_cross_entropy(y, y_pred)
--- supervised_learning/classification/tensorflow/5-create_train_op.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" train"""
-import tensorflow as tf
-
-
-def create_train_op(loss, alpha):
-    """ training operation"""
-    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)
