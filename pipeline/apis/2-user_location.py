c51c07bf23a9fd75f2b143315c6460f1068774a0
serge.kmz <127386891+sergekamanzi@users.noreply.github.com>
2025-02-27 19:25:08 +0200

    2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,39 +1,25 @@
 #!/usr/bin/env python3
 
-""" Return user location or rate limit status """
+
+""" Return list of ships"""
 
 import requests
 import sys
 import time
-import os
 
-def main():
-    if not os.access("./2-user_location.py", os.X_OK):
-        print("", end="")  # Simulating empty output for [Got]
-        print("timeout: failed to run command ‘./2-user_location.py’: Permission denied", file=sys.stderr)
-        return
-    
-    if len(sys.argv) < 2:
-        print("Usage: {} <url>".format(sys.argv[0]), file=sys.stderr)
-        return
-
-    try:
-        res = requests.get(sys.argv[1])
-        
-        if res.status_code == 403:
-            rate_limit = int(res.headers.get('X-Ratelimit-Reset', time.time()))
-            current_time = int(time.time())
-            diff = (rate_limit - current_time) // 60
-            print("Reset in {} min".format(diff))
-        elif res.status_code == 404:
-            print("Not found")
-        elif res.status_code == 200:
-            res = res.json()
-            print(res.get('location', ''))
-        else:
-            print("Unexpected response code: {}".format(res.status_code), file=sys.stderr)
-    except Exception as e:
-        print("Error: {}".format(str(e)), file=sys.stderr)
 
 if __name__ == "__main__":
-    main()
+    res = requests.get(sys.argv[1])
+
+    if res.status_code == 403:
+        rate_limit = int(res.headers.get('X-Ratelimit-Reset'))
+        current_time = int(time.time())
+        diff = (rate_limit - current_time) // 60
+        print("Reset in {} min".format(diff))
+        # get remaining rate
+
+    elif res.status_code == 404:
+        print("Not found")
+    elif res.status_code == 200:
+        res = res.json()
+        print(res['location'])
